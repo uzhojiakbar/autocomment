fe438d3e11737711268c1e78c84699e21c7d42db
uzhojiakbar <uzhojiakbar3@gmail.com>
2024-08-01 23:51:04 +0500

    Done

---
+++ main.py
@@ -0,0 +1,80 @@
+import os
+import random
+import asyncio
+from telethon import TelegramClient, events
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty
+from telethon.errors import FloodWaitError
+
+# Telegram API sozlamalari
+api_id = '13035049'
+api_hash = '1f08e6980689fa2e2d558f16a0d7bdcc'
+phone = '+998956466040'
+client = TelegramClient('account', api_id, api_hash)
+
+# Kanal va sharhlar fayllarini yuklash
+with open('channels.txt', 'r') as file:
+    channels = [line.strip() for line in file.readlines()]
+
+with open('kanal.txt', 'r') as file:
+    kanal = [line.strip() for line in file.readlines()]
+
+
+with open('texts.txt', 'r') as file:
+    comments = [line.strip() for line in file.readlines()]
+
+async def main():
+    await client.start(phone)
+    
+    # Mavjud kanallar ro'yxatini olish
+    dialogs = await client(GetDialogsRequest(
+        offset_date=None,
+        offset_id=0,
+        offset_peer=InputPeerEmpty(),
+        limit=200,
+        hash=0
+    ))
+
+    # Tekshirish uchun dialogs ma'lumotlarini chop etish
+    print("Dialogs: ", dialogs)
+
+    # Kanallarni filtrlash
+    channel_ids = [dialog.id for dialog in dialogs.chats if hasattr(dialog, 'username') and dialog.username in channels]
+    kanal_id = [dialog.id for dialog in dialogs.chats if hasattr(dialog, 'username') and dialog.username in kanal]
+
+    
+    # Filtrlangan kanallarni chop etish
+    print("Filtrlangan kanallar: ", channel_ids)
+
+    @client.on(events.NewMessage(chats=channel_ids))
+    async def handler(event):
+        try:
+            # Forward qilingan xabarlarni tekshirish
+            if event.message.fwd_from :
+                print(event.message.fwd_from.from_id.channel_id in kanal_id)
+                print("ID", channel_ids)
+                print("ID", event.message.fwd_from.from_id.channel_id)
+                print("Forward qilingan kanal xabari aniqlandi.")
+                comment = random.choice(comments)
+                try:
+                    await client.send_message(event.message.to_id, comment, reply_to=event.message.id)
+                    print(f"Sharh qo'shildi: {comment}")
+                except FloodWaitError as e:
+                    print(f"FloodWaitError: {e}. Kutish va qayta urinish.")
+                    await asyncio.sleep(e.seconds)
+                    await client.send_message(event.message.to_id, comment, reply_to=event.message.id)
+                    print(f"Sharh qo'shildi: {comment} (Qayta urinishdan keyin)")
+            else:
+                print("Forward qilingan xabar yoki kanal ID mos kelmadi.")
+        except Exception as e:
+            print(f"Xato: {e}")
+
+    print("Bot ishga tushdi...")
+    await client.run_until_disconnected()
+
+with client:
+    client.loop.run_until_complete(main())
+
+
+# MessageFwdHeader(
+#      from_id=PeerChannel(channel_id=2214802039), from_name=None, channel_post=30, post_author=None, saved_from_peer=PeerChannel(channel_id=2214802039), saved_from_msg_id=30, saved_from_id=None, saved_from_name=None, saved_date=None, psa_type=None)
